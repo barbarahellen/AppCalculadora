488ed66379db004664003eae11ef1dfb373a3ba2
Bárbara Hellen <barbarahellen993@gmail.com>
2023-10-31 16:43:58 -0300

    Initial commit

---
+++ src/main/java/br/com/bhps/AppCalculadora/resources/WebController.java
@@ -0,0 +1,52 @@
+package br.com.bhps.AppCalculadora.resources;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String showForm(Model model){
+        // adicionar uma lista de operações
+        model.addAttribute("modelOperacoes", List.of("Soma", "Subtracao"));
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String handleFormSubmission(@RequestParam String modelOperacoes,
+                                       @RequestParam String valor01,
+                                       @RequestParam String valor02,
+                                       Model model){
+
+        if(modelOperacoes.isEmpty() || valor01.isEmpty() || valor02.isEmpty())
+            return null;
+
+
+        String resposta = "";
+        int resp = 0;
+        switch(modelOperacoes){
+            case "Soma":
+                resp = Integer.parseInt(valor01) + Integer.parseInt(valor02);
+                resposta = String.valueOf(resp);
+                break;
+            case "Subtracao":
+                resp = Integer.parseInt(valor01) - Integer.parseInt(valor02);
+                resposta = String.valueOf(resp);
+                break;
+        }
+        resposta = String.valueOf(resp);
+
+        //devolver a resposta para a tela
+
+        model.addAttribute("modelOperacoes",List.of("Soma", "Subtracao"));
+        model.addAttribute("response", resposta);
+        model.addAttribute("selectedModel", modelOperacoes);
+        return "form";
+    }
+}
